32a3462fe7cbda9193f6c0bb85bd84812ccfb1f7
smm3123 <syedmahdi3123@gmail.com>
2020-08-01 17:16:34 -0500

    Add base client/server functionality

--- client.py
+++ client.py
@@ -7,3 +7,17 @@ Sources:
 1) https://realpython.com/python-sockets/
 2) https://docs.python.org/3.4/howto/sockets.html
 """
+
+import socket
+
+HOST = "127.0.0.1"
+PORT = 65432
+ADDRESS = (HOST, PORT)
+QUIT_MSG = "/q"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.connect(ADDRESS)
+    s.sendall(b"Test message")
+    data = s.recv(1024)
+
+print('Received', repr(data))
--- server.py
+++ server.py
@@ -7,3 +7,23 @@ Sources:
 1) https://realpython.com/python-sockets/
 2) https://docs.python.org/3.4/howto/sockets.html
 """
+
+import socket
+
+HOST = "127.0.0.1"  #socket.gethostbyname(socket.gethostname())
+PORT = 65432
+ADDRESS = (HOST, PORT)
+QUIT_MSG = "/q"
+
+with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as s:
+    s.bind(ADDRESS)
+    s.listen()
+    print("Server listening on: " + HOST + " on port " + str(PORT) + "...")
+    connection, address = s.accept()
+    with connection:
+        print("Connected by", address)
+        while True:
+            data = connection.recv(1024)
+            if not data:
+                break
+            connection.sendall(data)
