3f68651afc1818a88c19257fe44bf09ccef54f92
smm3123 <syedmahdi3123@gmail.com>
2020-08-01 22:28:26 -0500

    Update file header docstring

--- src/constants.py
+++ src/constants.py
@@ -1,7 +1,14 @@
 """
 Constants.py
 
+Name: Syed Mahdi
+Date: 8/1/2020
+Course: CS 372
+
 Constants used for both server and client files
+
+Sources:
+1) https://stackoverflow.com/questions/6343330/importing-a-long-list-of-constants-to-a-python-file
 """
 
 HOST = "localhost"
